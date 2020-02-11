8a4ffc6efb0ef1d80bb83d8f2bfb0ba3edaa7eab
Rolando Urquiza <rolurquiza@gmail.com>
2020-02-11 17:48:53 +0200

    Add signal to include new users in public staff group

--- user/apps.py
+++ user/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
 
+def add_to_public_staff_group(sender, instance, created, **kwargs):
+    from django.contrib.auth.models import Group
+    
+    if created:
+        group = Group.objects.get(name='public-staff')
+        instance.groups.add(group)
 
 class UserConfig(AppConfig):
     name = 'user'
+
+    def ready(self):
+        from django.db.models.signals import post_save
+        from django.conf import settings
+        
+        post_save.connect(add_to_public_staff_group, sender=settings.AUTH_USER_MODEL)
